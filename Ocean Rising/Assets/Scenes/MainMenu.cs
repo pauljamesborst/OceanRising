e568ec0610c9902c8e8825dac6f9103338afd971
angiereyes99 <angeloreyes1180@yahoo.com>
2021-03-12 20:41:21 -0800

    started mysql/php and login/register

---
+++ Ocean Rising/Assets/Scenes/MainMenu.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenu : MonoBehaviour
+{
+    public void GoRegister(string sceneName)
+    {
+        SceneManager.LoadScene(sceneName);
+    }
+
+}
---
+++ Ocean Rising/Assets/Scenes/Registration.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Registration : MonoBehaviour
+{
+    public InputField usernameField;
+    public InputField passwordField;
+
+    public Button submitButton;
+
+    public void CallRegister()
+    {
+        StartCoroutine(Register());
+    }
+
+    IEnumerator Register()
+    {
+        WWW www = new WWW("http://localhost/sqlconnect/register.php");
+    }
+}
