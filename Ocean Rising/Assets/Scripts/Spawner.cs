3f41e6af2be1fdb66452d7a09030a3213a5b5969
Paul Borst <pborst@mail.sfsu.edu>
2021-03-16 23:55:57 -0700

    Spawn food - Needs food "image" to replace placeholders.

---
+++ Ocean Rising/Assets/Scripts/Spawner.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Spawner : MonoBehaviour
+{
+    public GameObject[] food;
+    public Vector3 spawnValues;
+    public float spawnWait;
+    public float spawnMostWait;
+    public float spawnLeastWait;
+    public int startWait;
+    public bool stop;
+
+    int randFood;
+
+    void Start()
+    {
+        StartCoroutine(waitSpawner());
+    }
+
+    void Update()
+    {
+        spawnWait = Random.Range(spawnLeastWait, spawnMostWait);
+    }
+
+    IEnumerator waitSpawner()
+    {
+        yield return new WaitForSeconds(startWait);
+
+        while (!stop)
+        {
+            randFood = Random.Range(0, 2); //Randomly choose 1 of 2 possible foods
+
+            Vector3 spawnPosition = new Vector3(Random.Range(-spawnValues.x, spawnValues.x), Random.Range(-spawnValues.y, spawnValues.y), Random.Range(-spawnValues.z, spawnValues.z));
+
+
+            Instantiate(food[randFood], spawnPosition + transform.TransformPoint(0, 0, 0), gameObject.transform.rotation); //Spawn object at given location
+
+            yield return new WaitForSeconds(spawnWait);
+        }
+    }
+}
